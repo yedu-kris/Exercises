7a7bbca50d68eab931a20fcd6acf823aff787510
yedu-kris <62433787+yedu-kris@users.noreply.github.com>
2024-05-04 00:19:18 +0530

    Removed

--- src/main/java/com/exercise/question/Exercise3.java
+++ src/main/java/com/exercise/question/Exercise3.java
@@ -1,18 +1,19 @@
 package com.exercise.question;
 
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
     public static void main(String[] args) throws IOException {
-
-        // Use ReactiveSources.intNumbersFlux()
-
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
-
-
+        Mono<List<Integer>> mono = ReactiveSources.intNumbersFlux().collectList();
+        mono.subscribe(list->{
+            System.out.println(list+" "+list.size());
+        });
         System.out.println("Press a key to end");
         System.in.read();
     }
