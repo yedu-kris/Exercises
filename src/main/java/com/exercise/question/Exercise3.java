76ee302faa4fd140cd385d6bbb32318f52e90673
Yedu Krishnan P <yedukrishnanp@Yedus-MacBook-Air.local>
2024-05-04 00:07:28 +0530

    3rd

--- src/main/java/com/exercise/question/Exercise3.java
+++ src/main/java/com/exercise/question/Exercise3.java
@@ -1,12 +1,20 @@
 package com.exercise.question;
 
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
 
 public class Exercise3 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumbersFlux()
+        ReactiveSources.intNumbersFlux().collectList()
+        .subscribe(list-> {
+            System.out.println(list);
+            System.out.println(list.size());
+        });
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
