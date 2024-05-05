b27a282468380305a4cc201a0305c1bcaa115bbc
yedukrishnanp <yedukrishna334@gmail.com>
2024-05-05 11:36:12 +0530

    cleaned 4

--- src/main/java/com/exercise/question/Exercise4.java
+++ src/main/java/com/exercise/question/Exercise4.java
@@ -6,14 +6,10 @@ public class Exercise4 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use
-
-        // Print the value from intNumberMono when it emits
         ReactiveSources.intNumberMono().subscribe(
                 number-> System.out.println(number)
         );
-
-        // Get the value from the Mono into an integer variable
+        
         Integer v = ReactiveSources.intNumberMono().block();
         System.out.println(v);
         System.out.println("Press a key to end");
