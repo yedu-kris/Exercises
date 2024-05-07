a9e52a7483570ae72ff70e7a4ec07a229f1a93a9
yedukrishnanp <yedukrishna334@gmail.com>
2024-05-08 01:12:19 +0530

    exe-5,6

--- src/main/java/com/exercise/question/Exercise5.java
+++ src/main/java/com/exercise/question/Exercise5.java
@@ -1,6 +1,9 @@
 package com.exercise.question;
 
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class Exercise5 {
 
@@ -9,13 +12,42 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(
+                value-> System.out.println(value),
+                error-> System.err.println(error),
+                ()-> System.out.println("Flux Completed")
+        );
+
+        ReactiveSources.userMono().subscribe(
+                value-> System.out.println(value),
+                error-> System.err.println(error),
+                ()-> System.out.println("Flux Completed")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+
+        ReactiveSources.userMono().subscribe(new CustomSubscriber<>());
+
+
 
         System.out.println("Press a key to end");
         System.in.read();
+    }
+    static class CustomSubscriber<T> extends BaseSubscriber<T> {
+        @Override
+        protected void hookOnNext(T value) {
+            System.out.println("Received value: " + value);
+        }
+
+        @Override
+        protected void hookOnError(Throwable throwable) {
+            System.err.println("Error occurred: " + throwable.getMessage());
+        }
+
+        @Override
+        protected void hookOnComplete() {
+            System.out.println("Flux completed");
+        }
     }
 
 }
